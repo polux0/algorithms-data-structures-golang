27fd9f7fed3c90b5d91001d155a10a95c5ac043f
alexus <afidelo@protonmail.com>
2020-04-15 13:18:33 +0200

    minor

--- rotatearray.go
+++ rotatearray.go
@@ -2,8 +2,6 @@ package main
 
 import "fmt"
 
-// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-// [3, 4, 5, 6, 7, 8, 9, 10, 1, 2]
 func rotateArray(data []int, position int) {
 	length := len(data) - 1
 	reverseArray(data, 0, position-1)
